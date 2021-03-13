24243396d45fe0719368b9a7219bfa216b9fc734
Oscar Salvador Morillo Victoria <omorillovictoria@googlemail.com>
2021-03-13 15:59:42 +0100

    implement a first version of ecs deployment

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { ECS } = require('aws-sdk');
 
-try {
-	// `who-to-greet` input defined in action metadata file
-	const nameToGreet = core.getInput('who-to-greet');
-	console.log(`Hello ${nameToGreet}!`);
-	const time = (new Date()).toTimeString();
-	core.setOutput('time', time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
-} catch (error) {
-	core.setFailed(error.message);
-}
+const path = require('path');
+const fs = require('fs');
+
+const ecs = new ECS({ region: 'eu-central-1' });
+
+async function deployToECS() {
+
+	// read input params
+	const environment = core.getInput('environment', { required: true });
+	const version = core.getInput('version', { required: true });
+	const clusterName = core.getInput('clusterName', { required: true });
+	const serviceName = core.getInput('serviceName', { required: true });
+	const desiredCount = core.getInput('desiredCount');
+	const ecrRegistry = core.getInput('ecrRegistry', { required: true });
+
+	// check if taskdefinition build script is available
+	const taskDefPath = path.resolve('taskdefinition.js');
+	if (!fs.existsSync(taskDefPath)) {
+		throw new Error('No "taskdefinition.js" file found in project root');
+	}
+
+	// build and push docker image
+	const ecrRepo = `${ecrRegistry}:${imageName}`;
+
+	await exec.exec(`docker build -t ${serviceName} .`);
+	await exec.exec(`docker tag ${serviceName}:latest ${ecrRepo}:${version}`);
+	await exec.exec(`docker push ${ecrRepo}:${version}`);
+
+	// build and register new taskdefinition
+	const taskDefFunc = require(taskDefPath);
+	const args = taskDefFunc(environment, version);
+	const { taskDefinition } = await ecs.registerTaskDefinition(args)
+		.promise();
+	//console.log(JSON.stringify(taskDefinition, undefined, 2));
+	const taskdefinitionId = `${taskDefinition.family}:${taskDefinition.revision}`;
+
+	// deploy service
+	const updateParams = {
+		'cluster': clusterName,
+		'service': serviceName,
+		'taskDefinition': taskdefinitionId,
+		'forceNewDeployment': true,
+		'desiredCount': desiredCount,
+	};
+
+	const response = await ecs.updateService(updateParams)
+		.promise();
+	console.log(JSON.stringify(response));
+};
+
+deployToECS();
